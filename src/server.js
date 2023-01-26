e59b5f4d33cd51133322f6fee667de40ad4626fa
douglas-ssouza <douglasssouza1994@gmail.com>
2023-01-26 14:19:37 -0300

    update script

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const app = require('./app');
+const connection = require('../src/database/connection');
 
 const PORT = 3001;
 
-app.listen(PORT, () => { console.log(`Wallet API running on PORT ${PORT}`) });
+app.listen(PORT, async () => {
+  console.log(`Wallet API running on PORT ${PORT}`);
+
+  const [result] = await connection.execute('SELECT * FROM expenses');
+
+  console.log(result);
+});
--- wallet_script.sql
+++ wallet_script.sql
@@ -2,8 +2,16 @@ USE walletdb;
 
 CREATE TABLE expenses (
     id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-    dsecription VARCHAR(100) NOT NULL,
-    valor DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY(id),
+    `name` VARCHAR(45) NOT NULL,
+    `description` VARCHAR(100) NOT NULL,
+    `value` DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY(id)
 );
+
+INSERT INTO expenses 
+    (`id`,`name`,`description`,`value`) 
+VALUES 
+    (1, 'Mercado', 'pão e queijo', 28.00),
+    (2, 'Aluguel', 'aluguel + água + iptu', 1160.00),
+    (3, 'Luz', 'energia elétrica', 110.00),
+    (4, 'Água', '2 galões de água', 18.00);
