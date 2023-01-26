01c5a044948e01cbadd09ba68e37126ef9defbc1
douglas-ssouza <douglasssouza1994@gmail.com>
2023-01-26 17:26:23 -0300

    adds morgan lib

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 require('express-async-errors');
+const morgan = require('morgan');
 
 const expensesRouter = require('./routes/expensesRouter');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/expenses', expensesRouter);
 
