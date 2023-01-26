e03581a68178ce8efc365e62c9ec3945f8b99e28
douglas-ssouza <douglasssouza1994@gmail.com>
2023-01-26 16:31:54 -0300

    adds PUT /expenses/:id tests

--- tests/expenses.test.js
+++ tests/expenses.test.js
@@ -102,3 +102,41 @@ describe('Testing POST endpoints', function () {
       expect(response.body).to.deep.equal({ message: 'Expense successfully posted with id 53' });
   });
 });
+
+describe('Testing PUT endpoints', function () {
+  describe('When id is found', function () {
+    it('returns status 200', async function () {
+      const response = chai
+        .request(app)
+        .put('/expenses/1')
+        .send(expenses.NEW_EXPENSE);
+      expect(response).to.have.status(200);
+    });
+
+    it('returns message "Expense updated successfully"', async function () {
+      const response = chai
+        .request(app)
+        .put('/expenses/1')
+        .send(expenses.NEW_EXPENSE);
+      expect(response.body).to.deep.equal({ message: 'Expense updated successfully.' });
+    });
+  });
+
+  describe('When id is not found', function () {
+    it('returns status 404', async function () {
+      const response = chai
+        .request(app)
+        .put('/expenses/111')
+        .send(expenses.NEW_EXPENSE);
+      expect(response).to.have.status(404);
+    });
+
+    it('returns message "Expense not found."', async function () {
+      const response = chai
+        .request(app)
+        .put('/expenses/1')
+        .send(expenses.NEW_EXPENSE);
+      expect(response.body).to.deep.equal({ message: 'Expense not found.' });
+    });
+  });
+});
