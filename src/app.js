c085e14a51786916eb8e384b4f447a645c39c536
douglas-ssouza <douglasssouza1994@gmail.com>
2023-01-26 17:28:49 -0300

    adds cors lib

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const expensesRouter = require('./routes/expensesRouter');
 
@@ -8,7 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use('/expenses', expensesRouter);
+
+app.use((_req, res) => res.status(404).json({ message: 'Route not found.' }));
 
 module.exports = app;
